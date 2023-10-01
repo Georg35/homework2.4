906ca692420840417b22f4f6fd43bee5333975ac
Георгий <georgijstenin@gmail.com>
2023-10-01 19:05:08 +0500

    Дз 2.4 завершена!

---
+++ src/main/java/pro/sky/skyprohomework24/controller/FirstController.java
@@ -0,0 +1,40 @@
+package pro.sky.skyprohomework24.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import pro.sky.skyprohomework24.service.api.CalculatorService;
+
+@RestController
+public class FirstController {
+    private final CalculatorService calculatorService;
+
+    public FirstController(CalculatorService calculatorService) {
+        this.calculatorService = calculatorService;
+    }
+
+    @GetMapping(path = "/calculator")
+    public String showWelcome() {
+        return calculatorService.showWelcome();
+    }
+
+    @GetMapping(path = "/calculator/plus")
+    public String summa(@RequestParam("num1") int usnum1, @RequestParam("num2") int usnum2) {
+        return calculatorService.summa(usnum1, usnum2);
+    }
+
+    @GetMapping(path = "/calculator/ minus")
+    public String minus(@RequestParam("num1") int usnum1, @RequestParam("num2") int usnum2) {
+        return calculatorService.minus(usnum1, usnum2);
+    }
+
+    @GetMapping(path = "/calculator/multiply")
+    public String multiply(@RequestParam("num1") int usnum1, @RequestParam("num2") int usnum2) {
+        return calculatorService.multiply(usnum1, usnum2);
+    }
+
+    @GetMapping(path = "/calculator/divide")
+    public String divide(@RequestParam("num1") int usnum1, @RequestParam("num2") int usnum2) {
+        return calculatorService.divide(usnum1, usnum2);
+    }
+}
---
+++ src/main/java/pro/sky/skyprohomework24/service/impl/CalculatorServiceImpl.java
@@ -0,0 +1,33 @@
+package pro.sky.skyprohomework24.service.impl;
+
+import org.springframework.stereotype.Service;
+import pro.sky.skyprohomework24.service.api.CalculatorService;
+
+@Service
+public class CalculatorServiceImpl implements CalculatorService {
+
+    public String showWelcome() {
+        return "Добро пожаловать в калькулятор";
+    }
+
+    public String summa(int usnum1, int usnum2) {
+        return usnum1 + " + " + usnum2 + " = " + (usnum1 + usnum2);
+    }
+
+    public String minus(int usnum1, int usnum2) {
+        return usnum1 + " - " + usnum2 + " = " + (usnum1 - usnum2);
+    }
+
+    public String multiply(int usnum1, int usnum2) {
+        return usnum1 + " * " + usnum2 + " = " + (usnum1 * usnum2);
+    }
+
+    public String divide(int usnum1, int usnum2) {
+        if (usnum2 == 0) {
+            return "Ошибка: на ноль делить нельзя!";
+        } else
+            return usnum1 + " / " + usnum2 + " = " + (usnum1 / usnum2);
+
+        }
+    }
+
